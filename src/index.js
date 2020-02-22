352bb31fe367585cc784a68564a32b35caf06cec
803142 <803142@gmail.com>
2020-02-23 00:36:16 +0300

    feat: matrix sorted

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix?
+            matrix.reduce((acc, cur, idx)=>
+              !(idx%2)?acc=[...acc,...cur.sort((a, b)=>a-b)]
+              :acc=[...acc,...cur.sort((a, b)=>b-a)]
+            ,[])
+            :[];
 }
